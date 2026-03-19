80eb215139652869bb7ec2abb042cdb9be4496c4
Etoile2006 <e.badaba@edu.umi.ac.ma>
2026-03-19 17:51:22 +0100

    fix error

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,17 +1,16 @@
 import sys
 import os
-
-from app.tasks import add_task, get_tasks
-
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../app")))
 
+from tasks import add_task, get_tasks
+
 def test_add_task():
-    add_task("learn Docker")
-    assert "learn Docker" in get_tasks()
+    add_task("Learn Docker")
+    assert "Learn Docker" in get_tasks()
+
 def test_multiple_tasks():
-    add_task("learn CI")
-    add_task("learn Devops")
-    
-    tasks=get_tasks()
-    assert "learn CI" in tasks
-    assert "learn Devops" in tasks
+    add_task("Learn CI")
+    add_task("Learn DevOps")
+    tasks = get_tasks()
+    assert "Learn CI" in tasks
+    assert "Learn DevOps" in tasks
